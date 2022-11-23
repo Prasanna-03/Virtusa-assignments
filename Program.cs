e5c3776e00a52b7b3eaa4fd8c77063ca893714e9
K.Prasanna Reddy <97664454+Prasanna-03@users.noreply.github.com>
2022-11-23 10:13:27 +0530

    4. c sharp program on arrays

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿int k,n;
+k = Convert.ToInt32(Console.ReadLine());
+n = Convert.ToInt32(Console.ReadLine());
+int[] arr ={0,0,0,0,0,0,0,0,0,0};
+Console.WriteLine("Enter the array elements:");
+for(int i =0;i<n;i++)
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    if(a<k)
+    {
+        a=a+2;
+    }
+    else
+    {
+        a=a-2;
+    }
+    arr[i]=a;
+    
+}
+
+int low = arr[0];
+int high = arr[0];
+for(int i =1;i<n;i++)
+{
+    if (low > arr[i])
+    {
+        low = arr[i];
+    }
+    if (high < arr[i])
+    {
+        high = arr[i];
+    }
+}
+Console.WriteLine(high-low);
+
